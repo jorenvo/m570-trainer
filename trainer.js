7cdfdb10cb93d2d425deedd1ea246642d082e3dd
Joren Van Onder <joren.vanonder@gmail.com>
2016-05-31 18:49:50 +0200

    reset state

--- trainer.js
+++ trainer.js
@@ -1,6 +1,14 @@
 var AMOUNT_TARGETS = 10;
-var start_time;
+var start_time = undefined;
 var clicked_targets = 0;
+
+// average with regular mouse is 9.5s
+
+function reset_state () {
+    $("#click_me").css({top: 5, left: 5});
+    clicked_targets = 0;
+    start_time = undefined;
+}
 
 function reposition_target () {
     var new_random_top = Math.floor(Math.random() * 900);
@@ -9,13 +17,17 @@ function reposition_target () {
 
     clicked_targets++;
 
+    if (! start_time) {
+        start_time = new Date();
+    }
+
     if (clicked_targets >= AMOUNT_TARGETS) {
         alert("Clicked " + AMOUNT_TARGETS + " in " + (new Date() - start_time) / 1000 + " seconds");
+        reset_state();
     }
 }
 
 $(document).ready(function () {
-    start_time = new Date();
     $("#click_me").on("click", reposition_target);
 });
 
